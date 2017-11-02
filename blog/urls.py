b75f6077fcd69d9d462f2f6d9269d611aabafb5a
cning <tatung1227@gmail.com>
2017-11-02 17:39:37 +0800

    add new edit functions

--- blog/urls.py
+++ blog/urls.py
@@ -6,5 +6,8 @@ urlpatterns = [
         url(r'^$', views.post_list, name='post_list'),
         # ?P<pk> take the followings as the value of variable pk
         # this pk is the argument of post_detail...
+        # the name is seen by the html {% url 'post_detail' arg... %}
         url(r'^post/(?P<pk>[0-9]+)/$', views.post_detail, name='post_detail'),
+        url(r'^post/new/$', views.post_new, name='post_new'),
+	url(r'^post/(?P<pk>[0-9]+)/edit/$', views.post_edit, name='post_edit'),
 ]

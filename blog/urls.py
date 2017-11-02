cede95fc9b93951960c312b9d41f800915fb5f9d
cning <tatung1227@gmail.com>
2017-11-02 16:28:36 +0800

    add template and css

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,10 @@
 from django.conf.urls import url
 from . import views
 
+# any url like r'...', call views.bla and response.
 urlpatterns = [
         url(r'^$', views.post_list, name='post_list'),
+        # ?P<pk> take the followings as the value of variable pk
+        # this pk is the argument of post_detail...
+        url(r'^post/(?P<pk>[0-9]+)/$', views.post_detail, name='post_detail'),
 ]
